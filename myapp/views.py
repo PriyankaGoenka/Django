a0d8a2b42e0cb0b65a22c6cfc801c87669b839c0
Priyanka Goenka <goenkapr@msu.edu>
2021-03-29 01:00:29 -0400

    Django

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+def signup(request):
+    return render(request,'myapp/signup.html')
 def home(request):
-    return HttpResponse("Hello! Welcome to the Home page.")
+    return render(request,'myapp/home.html')
+def about(request):
+    return HttpResponse("This is about us page")
+def contact(request):
+    return HttpResponse("Contact us here")
+ 
